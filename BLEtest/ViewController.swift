4fda1d146b19bc43ddd76c0f0c6dd4bdbec9d3d2
Alexandru Bordei <alexandru.bordei@bigstep.com>
2018-01-11 20:56:06 +0200

    Initial Commit

---
+++ BLEtest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BLEtest
+//
+//  Created by Alexandru Bordei on 1/11/18.
+//  Copyright © 2018 Alexandru Bordei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
